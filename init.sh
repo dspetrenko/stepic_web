24b9ec0b113de5046b086d8f756de910eee5aea7
Denis Petrenko <denis.petrenko.dp@gmail.com>
2018-01-11 17:08:29 +0300

    fix: change config name

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/nginx.conf
+sudo ﻿ln -s /home/box/web/etc/project.conf  /etc/nginx/sites-enabled/project.conf
 sudo /etc/init.d/nginx restart

eff1708f1db779cc17c70d0686dc2ef8ae6e80b5
Denis Petrenko <denis.petrenko.dp@gmail.com>
2018-01-13 22:22:54 +0300

    remove default config in sites-av

--- init.sh
+++ init.sh
@@ -1,2 +1,2 @@
-sudo ﻿ln -s /home/box/web/etc/project.conf  /etc/nginx/sites-enabled/project.conf
+sudo ﻿ln -sf /home/box/web/etc/project.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart

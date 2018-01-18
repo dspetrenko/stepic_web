d2779a76a8f84193ef98b5542074511c2354555e
Denis Petrenko <denis.petrenko.dp@gmail.com>
2018-01-18 22:57:55 +0300

    add template to wsgi app

---
+++ hello.py
@@ -0,0 +1,16 @@
+
+
+def wsgi_app(environ, start_response):
+
+    status = '200 OK'
+    headers = [
+        ('Content-Type', 'text/plain'),
+    ]
+
+    start_response(status, headers)
+
+    query_string = environ.get('QUERY_STRING')
+    print(query_string)
+
+    return []
+

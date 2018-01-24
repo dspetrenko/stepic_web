d973c1bb44cc9e061ceb86a792eeba2f987ab290
Denis Petrenko <denis.petrenko.dp@gmail.com>
2018-01-24 21:11:44 +0300

    add start gunicorn

--- init.sh
+++ init.sh
@@ -1,2 +1,3 @@
-sudo ﻿ln -sf /home/box/web/etc/project.conf  /etc/nginx/sites-enabled/default
+sudo ﻿ln -sf /home/box/web/etc/gunicorn.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+sudo gunicorn -b 0.0.0.0:8080 hello:wsgi_app

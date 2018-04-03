e2f60948804f31bb33bc5708b1e651f11a8aaa52
Denis Petrenko <denis.petrenko.dp@gmail.com>
2018-04-03 17:59:38 +0300

    add puplar view

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from .views import test
 from .views import main
+from .views import popular
 
 
 urlpatterns = [
     path('', main),
+    path('popular/', popular),
     path('login/', test),
     path('signup/', test),
     path('question/<int:id>/', test),

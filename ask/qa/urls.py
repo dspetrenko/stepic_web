65d471a48d1dfdb5c0b71a247ee454de88726f88
Denis Petrenko <denis.petrenko.dp@gmail.com>
2018-04-03 18:53:27 +0300

    add question view to urls

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import test
 from .views import main
 from .views import popular
+from .views import queation
 
 
 urlpatterns = [
@@ -10,7 +11,7 @@ urlpatterns = [
     path('popular/', popular),
     path('login/', test),
     path('signup/', test),
-    path('question/<int:id>/', test),
+    path('question/<int:id>/', queation),
     path('ask/', test),
     path('popular/', test),
     path('new/', test),

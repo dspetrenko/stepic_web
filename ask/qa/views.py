1d1ccf861ef5acbf927995fd914117cd3344a32e
Denis Petrenko <denis.petrenko.dp@gmail.com>
2018-04-06 11:50:40 +0300

    ask view: add show form

--- ask/qa/views.py
+++ ask/qa/views.py
@@ -2,11 +2,14 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 
 from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 
 from django.core.paginator import Paginator
 
 from .models import Question as Q
 from .models import Answer as A
+
+from .forms import AskForm
 
 
 def test(request, *args, **kwargs):
@@ -52,4 +55,26 @@ def question(request, question_id):
 
 
 def ask(request):
-    return HttpResponse('It is in dev now.')
+
+    if request.method == 'POST':
+        form = AskForm(request.POST)
+
+        if form.is_valid():
+            new_question = form.save(commit=False)
+
+            from django.contrib.auth.models import User
+
+            new_question.author = User.objects.get(id=1)
+            new_question.save()
+
+            return HttpResponseRedirect(new_question.get_absolute_url())
+
+    else:
+        form = AskForm()
+
+    context = {
+        'form': form,
+    }
+
+    # return HttpResponse('It is in dev now.')
+    return render(request, 'ask.html', context=context)

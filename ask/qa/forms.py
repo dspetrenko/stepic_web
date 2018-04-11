9391a87dfd2bba84c32024093a33aa32bc543ee4
Denis Petrenko <denis.petrenko.dp@gmail.com>
2018-04-11 17:51:28 +0300

    fix Signup form

--- ask/qa/forms.py
+++ ask/qa/forms.py
@@ -4,7 +4,9 @@ from django.forms import ModelForm
 from django.forms import Form
 
 from django.forms.widgets import TextInput
-from django.forms.widgets import PasswordInput
+from django.forms.widgets import PasswordInput as PasswordInputWidget
+
+from django.forms.fields import CharField
 
 
 from .models import Question, Answer
@@ -30,6 +32,6 @@ class AnswerForm(ModelForm):
 
 class Signupform(Form):
 
-    username = TextInput()
-    email = TextInput()
-    password = PasswordInput()
+    username = CharField()
+    email = CharField()
+    password = CharField(widget=PasswordInputWidget)

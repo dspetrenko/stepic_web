48dee0ced41db68ce15ceee4f09b3e3aad8d7509
Denis Petrenko <denis.petrenko.dp@gmail.com>
2018-04-12 11:48:57 +0300

    add login view to urls

--- ask/qa/urls.py
+++ ask/qa/urls.py
@@ -6,12 +6,13 @@ from .views import popular
 from .views import question
 from .views import ask
 from .views import signup
+from .views import login_view
 
 
 urlpatterns = [
     path('', main),
     path('popular/', popular),
-    path('login/', test),
+    path('login/', login_view),
     path('signup/', signup),
     path('question/<int:question_id>/', question),
     path('ask/', ask),
